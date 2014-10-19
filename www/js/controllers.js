466e42b05a82c99ef7a84d14708921d8213bd149
gsaqui <glenn.saqui@gmail.com>
2014-10-18 22:32:50 -0700

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,35 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
+
+
+.controller('LoginCtrl', function($scope) {
+  $scope.loginData = {};
+
+  var dataRef = new Firebase("https://ionic-firebase-login.firebaseio.com/");
+
+
+  $scope.tryLogin = function() {
+    $scope.loginObj.$login('facebook').then(function(user) {
+      // The root scope event will trigger and navigate
+    }, function(error) {
+      // Show a form error here
+      console.error('Unable to login', error);
+    });
+  };
+})
+
+.controller('SignupCtrl', function($scope) {
+})
